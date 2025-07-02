c95bd5ebda0f724d66c927f5349053761beacf7f
tiedsandi <fachransandi@gmail.com>
2025-07-02 13:36:55 +0700

    [GSAP] Add Menu component with GSAP animations and integrate into App

--- gsap-mojito/src/App.jsx
+++ gsap-mojito/src/App.jsx
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Art from "./components/Art";
 import Cocktails from "./components/Cocktails";
 import Hero from "./components/Hero";
+import Menu from "./components/Menu";
 import Navbar from "./components/navbar";
 import gsap from "gsap";
 
@@ -17,6 +18,7 @@ const App = () => {
       <Cocktails />
       <About />
       <Art />
+      <Menu />
     </main>
   );
 };
